7165fdc2c69a80b840273f387d6413abc095dcb6
juan dolce <jdolce@frba.utn.edu.ar>
2023-05-25 22:02:26 -0300

    Cambio la ruta del archivo a una multiplataforma

--- src/main/java/ar/edu/utn/frba/dds/domain/funcionalidadRegistroUsuarios/ValidadorPeorContrasenia.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/funcionalidadRegistroUsuarios/ValidadorPeorContrasenia.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.charset.StandardCharsets;
 
 public class ValidadorPeorContrasenia {
@@ -17,22 +19,27 @@ public class ValidadorPeorContrasenia {
   @Getter
   private static ValidadorPeorContrasenia INSTANCE = new ValidadorPeorContrasenia();
 
-  private File abrirArchivo() {
+  /*private File abrirArchivo() {
     /*
     Una forma de abrirlo para distintos sistemas operativos es la siguiente:
     return new File(getClass().getResource("contraseniasPeligrosas.txt").getPath());
     pero no me funca no se porque, dice que no encuentra el archivo. Esta indexado en el IDE que resources es
     para dejar archivos que se utilizan en estos casos. No se porque tira ese error.
-    */
+
     return new File(this.rutaPeoresContrasenias);
-  }
+
+  }*/
 
   public void validarPosiblePeorContrasenia(String contrasenia) {
     try{
-      File archivoContraseniasPeligrosas = this.abrirArchivo();
+      //File archivoContraseniasPeligrosas = this.abrirArchivo();
+
+      String resourcePath = "contraseniasPeligrosas.txt";
+      Path path = Paths.get("src", "main", "resources", resourcePath);
+      String absolutePath = path.toAbsolutePath().toString();
 
       BufferedReader br = new BufferedReader(
-          new InputStreamReader(new FileInputStream(archivoContraseniasPeligrosas),
+          new InputStreamReader(new FileInputStream(absolutePath),
               StandardCharsets.UTF_8));
 
       String linea;
