71710a9d084b31decc58889773de1c3db7a2b142
Zoe Golfieri <gzoe@frba.utn.edu.ar>
2023-05-27 19:01:51 -0300

    Agregamos API

--- src/main/java/com/georef/GeoRefApi.java
+++ src/main/java/com/georef/GeoRefApi.java
@@ -1,5 +1,20 @@
 package com.georef;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import ar.edu.utn.frba.dds.domain.localizacion.ListadoDepartamentos;
+import ar.edu.utn.frba.dds.domain.localizacion.ListadoMuncipios;
+import ar.edu.utn.frba.dds.domain.localizacion.ListadoProvincias;
+
 public interface GeoRefApi {
+  @GET("provincias")
+  Call<ListadoProvincias> provincias();
+
+  @GET("municipios")
+  Call<ListadoMuncipios> municipiosDeProvincia(@Query("provincia") int idProvincia, @Query("max") int max);
+
+  @GET("departamentos")
+  Call<ListadoDepartamentos> departamentosDeProvincia(@Query("provincia") int idProvincia, @Query("max") int max);
 
 }
