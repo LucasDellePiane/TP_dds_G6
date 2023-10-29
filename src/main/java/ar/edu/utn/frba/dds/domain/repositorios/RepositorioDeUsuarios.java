926381db8c084ec0dd75df3b9a0e68bfa1973b6c
LuchoAle <lrodriguez@teknosgroup.com>
2023-10-28 21:46:04 -0300

    agregamos card de comunidad con incidentes y logica en demo

--- src/main/java/ar/edu/utn/frba/dds/domain/repositorios/RepositorioDeUsuarios.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/repositorios/RepositorioDeUsuarios.java
@@ -1,20 +1,23 @@
 package ar.edu.utn.frba.dds.domain.repositorios;
 
+import ar.edu.utn.frba.dds.domain.establecimiento.Establecimiento;
 import ar.edu.utn.frba.dds.domain.usuario.Usuario;
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 import lombok.Getter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RepositorioDeUsuarios {
+public class RepositorioDeUsuarios implements WithSimplePersistenceUnit {
   @Getter
   private List<Usuario> usuariosDeLaPlataforma = new ArrayList<>();
 
   @Getter
-  private static RepositorioDeUsuarios INSTANCE = new RepositorioDeUsuarios();
+  private static final RepositorioDeUsuarios INSTANCE = new RepositorioDeUsuarios();
   public void aniadirUsuario(Usuario usuario) {
     usuariosDeLaPlataforma.add(usuario);
   }
-
-
+  public Usuario findById(Integer id) {
+    return (Usuario) usuariosDeLaPlataforma.stream().filter(usuario -> usuario.getId_usuario().equals(id));
+  }
 
 }
--- src/main/java/controller/DemoController.java
+++ src/main/java/controller/DemoController.java
@@ -1,19 +1,40 @@
 package controller;
 
+import ar.edu.utn.frba.dds.domain.Comunidad.Comunidad;
+import ar.edu.utn.frba.dds.domain.repositorios.RepositorioComunidad;
 import ar.edu.utn.frba.dds.domain.repositorios.RepositorioDeEntidades;
+import ar.edu.utn.frba.dds.domain.repositorios.RepositorioDeUsuarios;
 import ar.edu.utn.frba.dds.domain.repositorios.RepositorioEstablecimientos;
+import ar.edu.utn.frba.dds.domain.servicio.EstadoIncidente;
+import ar.edu.utn.frba.dds.domain.servicio.Incidente;
+import ar.edu.utn.frba.dds.domain.usuario.Usuario;
 import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DemoController implements WithSimplePersistenceUnit {
 
   public ModelAndView home(Request request, Response response) {
     Map<String, Object> modelo = new HashMap<>();
-    modelo.put("establecimientos", RepositorioEstablecimientos.getInstancia().obtenerTodos());
+//    Integer id = request.session().attribute("user_id");
+//    Usuario usuario = RepositorioDeUsuarios.getINSTANCE().findById(id);
+//    List<Comunidad> comunidades = RepositorioComunidad.getInstancia().comunidadesALasQuePertenece(usuario);
+//    List<List<Incidente>> listaincidentes = comunidades.stream().map(comunidad -> {
+//      EstadoIncidente estado = EstadoIncidente.valueOf(request.queryParams("estadoIncidentes"));;
+//      return comunidad.consultarIncidentesPorEstado(estado);
+//    }).toList();
+//    modelo.put("incidentes", listaincidentes);
     return new ModelAndView(modelo, "index.html.hbs");
+  }
+
+  public ModelAndView establecimientos(Request request, Response response) {
+    Map<String, Object> modelo = new HashMap<>();
+    modelo.put("establecimientos", RepositorioEstablecimientos.getInstancia().obtenerTodos());
+    return new ModelAndView(modelo, "index.html.hbs"); // cambiar esto del index
   }
 }
